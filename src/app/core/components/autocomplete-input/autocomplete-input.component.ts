aacc81cd1c3ebb23fbefe0229dce8c9ee0841876
Juan L Briseño <lopezjuankar@gmail.com>
2022-03-16 19:37:10 -0700

    setInput

--- src/app/core/components/autocomplete-input/autocomplete-input.component.ts
+++ src/app/core/components/autocomplete-input/autocomplete-input.component.ts
@@ -1,16 +1,25 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { OnInit, AfterViewInit, Component, ElementRef, ViewChild, Input } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { debounceTime, map } from 'rxjs/operators';
 import { ApiService } from '../../services/api.service';
+
+export interface configObject {
+  model: string;
+}
 
 @Component({
   selector: 'app-autocomplete-input',
   templateUrl: './autocomplete-input.component.html',
   styleUrls: ['./autocomplete-input.component.scss']
 })
-export class AutocompleteInputComponent implements OnInit {
+export class AutocompleteInputComponent implements OnInit, AfterViewInit {
   @ViewChild('searchInput', { static: true }) searchInput: ElementRef | undefined;
+  @Input()
+  set config(config: configObject) {
+    this.configObject = {...config};
+  }
 
+  public configObject: any = {};
   public options: any = [];
   public loading: boolean = false;
 
@@ -19,21 +28,24 @@ export class AutocompleteInputComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
     this.setListener();
   }
 
   setListener() {
     fromEvent(this.searchInput?.nativeElement, 'keyup')
-    .pipe(
-      debounceTime(500),
-      map((event: any) => event.target.value)
-    )
-    .subscribe(value => this.searchData(value));
+      .pipe(
+        debounceTime(500),
+        map((event: any) => event.target.value)
+      )
+      .subscribe(value => this.searchData(value));
   }
 
   async searchData(searchValue: string) {
     this.loading = true;
-    this._apiService.getData('issues', searchValue)
+    this._apiService.getData(this.configObject.model, searchValue)
       .then((data: any) => {
         this.options = data?.items || [];
         this.loading = false;
